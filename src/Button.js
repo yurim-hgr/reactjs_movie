f8fb89ae9318eda275f17fa4cb13348ac6e1d2ba
yurim-hgr <dbfla4618@naver.com>
2022-03-16 11:28:12 +0900

    first commit

---
+++ src/Button.js
@@ -0,0 +1,16 @@
+import styles from './Button.module.css';
+
+
+function Button ({text}) {
+    return (
+        <button className = {styles.btn}>
+            {text}
+        </button>
+    );
+
+    
+};
+
+
+
+export default Button;
