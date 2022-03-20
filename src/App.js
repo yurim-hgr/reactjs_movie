716ba0263d9ad7a008122703cf53e450050b6b01
yurim-hgr <dbfla4618@naver.com>
2022-03-20 21:16:22 +0900

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import { useEffect, useState } from "react";
 
 
-function Hello () {
-  
-  useEffect(() => {
-    console.log("created")
-    return () => console.log("des")
-    }, []);
-  return <h1>hihihi</h1>
-}
 
 function App() {
   
-  const [showing, setShowing] = useState(false);
-  const onclick = () => {
-    setShowing((prev) => !prev);
-  };
+  const [todo , setTodo] = useState("");
+  const [todos , setTodos ] = useState([]);
+  const onchange = (event) => {
+    setTodo(event.target.value);
+  }
+
+  const onsubmit = (e) => {
+    e.preventDefault();
+    if (todo === ""){
+      return;
+    }
+    setTodo("");
+    setTodos((currentArray) => [todo , ...currentArray]);
+    console.log(todos);
+    
+  }
   return (
     <div>
-      {showing ?  <Hello /> : null}
-      <button onClick={onclick}>{showing? "hide" : "show"}</button>
+      <h1>My Todos Docs({todos.length})</h1>
+      <form onSubmit={onsubmit}>
+      <input onChange={onchange} value={todo} type="text" placeholder= "write your todo"></input>
+      <button>add To Do</button>
+      </form>
+      < hr/>
+      <ul>
+        {todos.map((item , index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
